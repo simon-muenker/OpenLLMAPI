92d45947b8e8c328fdf562438ad07e46d537e7b3
Münker, Simon <muenker@uni-trier.de>
2024-04-11 17:42:52 +0200

    replace fastapi cors with starlette

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import typing
 
 import ollama
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 import config
 import schemas
