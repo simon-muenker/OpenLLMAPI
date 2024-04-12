585a6d92c95b44f219cfab63e2161ea205c09d5d
Münker, Simon <muenker@uni-trier.de>
2024-04-12 11:04:17 +0200

    fix load path pattern

--- api.py
+++ api.py
@@ -148,7 +148,7 @@ async def get_responses() -> typing.List[schemas.Response]:
 @app.get("/responses/{idx}", tags=['data'])
 async def update_item(idx: uuid.UUID) -> schemas.Response:
     item: typing.List[schemas.Response] = util.pydantic_from_glob(
-        f'{CFG.response_log_path}/{idx}', schemas.Response
+        f'{CFG.response_log_path}/{idx}.json', schemas.Response
     )
 
     if not item:
