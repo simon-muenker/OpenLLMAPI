7f3497ed626ff27b5085645f3882c5a64ddb7c5b
Münker, Simon <muenker@uni-trier.de>
2024-04-19 09:19:52 +0200

    add llama3 to request schema

--- schemas/requests/_request.py
+++ schemas/requests/_request.py
@@ -8,6 +8,7 @@ class _Request(pydantic.BaseModel):
         'falcon:40b-instruct-q5_1',
         'gemma:7b-instruct-q6_K',
         'llama2:70b-chat-q6_K',
+        'llama3:70b-instruct-q6_K',
         'mistral:7b-instruct-v0.2-q6_K',
         'mixtral:8x7b-instruct-v0.1-q6_K',
         'qwen:72b-chat-v1.5-q6_K',
